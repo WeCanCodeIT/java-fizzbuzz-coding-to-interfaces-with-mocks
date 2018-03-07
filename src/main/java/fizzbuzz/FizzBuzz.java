25546e9450952f47ae235d5f2c50899654f05ef1
B T Forsythe <brian@btforsythe.io>
2018-03-07 10:21:25 -0500

    basic fizzbuzz

---
+++ src/main/java/fizzbuzz/FizzBuzz.java
@@ -0,0 +1,26 @@
+package fizzbuzz;
+
+public class FizzBuzz {
+
+	public String say(int number) {
+		if(isFizz(number) && isBuzz(number)) {
+			return "fizzbuzz";
+		}
+		if(isFizz(number)) {
+			return "fizz";
+		}
+		if(isBuzz(number)) {
+			return "buzz";
+		}
+		return "" + number;
+	}
+
+	public boolean isFizz(int number) {
+		return number % 3 == 0;
+	}
+
+	public boolean isBuzz(int number) {
+		return number % 5 == 0;
+	}
+
+}
---
+++ src/test/java/fizzbuzz/FizzBuzzTest.java
@@ -0,0 +1,71 @@
+package fizzbuzz;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+import org.junit.Test;
+
+public class FizzBuzzTest {
+
+	@Test
+	public void shouldSayOne() {
+		FizzBuzz underTest = new FizzBuzz();
+		
+		String response = underTest.say(1);
+		
+		assertThat(response, is("1"));
+	}
+	
+	@Test
+	public void shouldSayTwo() {
+		FizzBuzz underTest = new FizzBuzz();
+		
+		String response = underTest.say(2);
+		
+		assertThat(response, is("2"));
+	}
+	
+	@Test
+	public void shouldSayFizzForThree() {
+		FizzBuzz underTest = new FizzBuzz();
+		
+		String response = underTest.say(3);
+		
+		assertThat(response, is("fizz"));
+	}
+	
+	@Test
+	public void shouldSayFizzForSix() {
+		FizzBuzz underTest = new FizzBuzz();
+		
+		String response = underTest.say(6);
+		
+		assertThat(response, is("fizz"));
+	}
+	
+	@Test
+	public void shouldSayBuzzForFive() {
+		FizzBuzz underTest = new FizzBuzz();
+		
+		String response = underTest.say(5);
+		
+		assertThat(response, is("buzz"));
+	}
+	
+	@Test
+	public void shouldSayBuzzForTen() {
+		FizzBuzz underTest = new FizzBuzz();
+		
+		String response = underTest.say(10);
+		
+		assertThat(response, is("buzz"));
+	}
+	
+	@Test
+	public void shouldSayFizzBuzzForFifteen() {
+		FizzBuzz underTest = new FizzBuzz();
+		
+		String response = underTest.say(15);
+		
+		assertThat(response, is("fizzbuzz"));	}
+}
